15f5834306f049fb22ac474f8faf8e8545431e71
PrijalBista <pri1.zzal@gmail.com>
2019-09-08 12:27:59 +0545

    Simple scalable express server implementation using cluster Master-worker architecture Master controls the creation of workers and handles crashing workers (creates new one) according to system capacity Multiple workers listen on the same port for request and serve.

---
+++ index.js
@@ -0,0 +1,38 @@
+// Scalable Express Server with favourable clusters
+
+const cluster = require('cluster');
+
+if (cluster.isMaster) {
+    
+    let numWorkers = require('os').cpus().length;
+    
+    for(let i=0; i<numWorkers; i++) {
+        cluster.fork();  // spawn multiple clusters
+    }
+
+    // handle cluster events
+
+    cluster.on('online', worker => {
+        console.log(`Process ${worker.process.pid} is online`);
+    });
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Process ${worker.process.pid} died with code ${code} and signal ${signal}`);
+        console.log('Starting a new worker');
+        client.fork();
+    });
+
+} // End of Master Code
+
+else  {
+    // client work
+    const app = require('express')();
+    
+    app.get('/', (req, res) => {
+        res.send(`Hello From Process ${process.pid} =)`).end();
+    });
+
+    server = app.listen(8000, () => {
+        console.log(`Process ${process.pid} listening to all incoming requests`);
+    });
+}
